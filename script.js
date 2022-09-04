19dbaf3d3f46e9f94730feb6706277181e3687ba
LuckyDnepr <LuckyDnepr@gmail.com>
2022-09-05 01:40:21 +0300

    Problem with sortying buttons

--- script.js
+++ script.js
@@ -13,17 +13,31 @@ const fetchingFields = {
     nat: false,
 };
 
-const baseURL = "https://randomuser.me/api/?inc=";
-const nOfUsers = 10;
+const actionsButtonsImages = [
+    {
+        "id": "user_actions_chat",
+        "src": "./icons/icon-chat.png"
+    },
+    {
+        "id": "user_actions_add",
+        "src": "./icons/icon-add-friend.png"
+    },
+    {
+        "id": "user_actions_preview",
+        "src": "./icons/icon-preview.png"
+    }
+];
+
+const baseURL = "https://randomuser.me/api/";
+const nOfUsers = 24;
 let usersData;
 
 const requestUrl = createRequestUrl(baseURL, fetchingFields);
 
-getUsers(createRequestUrl(baseURL, fetchingFields), nOfUsers);
-
 function createRequestUrl(baseURL, fieldsObject) {
     return (
         baseURL +
+        "?inc=" +
         Object.keys(fieldsObject).reduce((addition, key) => {
             if (fieldsObject[key]) {
                 addition += "," + key;
@@ -37,7 +51,7 @@ async function getUsers(url, numberOfUsers) {
     url += `&results=${+numberOfUsers}&nat=ua`;
     console.log(url);
     usersData = (await getData(url)).results;
-    console.log(usersData);
+
 }
 
 async function getData(requestUrl) {
@@ -50,8 +64,106 @@ async function getData(requestUrl) {
     }
 }
 
+async function searchFriends () {
+    await getUsers(createRequestUrl(baseURL, fetchingFields), nOfUsers);
+    renderUserCards(usersData, document.querySelector(".found_users"));
+    console.log(usersData);
+    document.querySelector(".lds-ripple").classList.toggle("hide");
+}
+
+function renderUserCards (usersData, container) {
+    container.innerHTML = usersData
+    .map(user => {
+        return `<div class="user_card">` +
+        createUserCardAvatarHTML(user) +
+        createUserCardLessInfo(user) +
+        createUserCardActionsButtons() +
+        createUserCardMoreInfo(user) +
+        `</div>`;
+    })
+    .join("");
+}
+
+function createUserCardAvatarHTML (user) {
+    return `<img
+            src="${user.picture.large}"
+            alt=""
+            class="user_avatar"/>`;
+}
+
+function createUserCardLessInfo (user) {
+    let gSymbol = "";
+    switch (user.gender) {
+        case "female":
+            gSymbol = "&#9792";
+            break;
+        case "male":
+            gSymbol = "&#9794";
+            break;
+        default:
+            break;
+    }
+    return `<div class="less_user_info">
+            <p class="user_nickname">
+            ${user.login.username} ${gSymbol};
+            </p>
+            <p class="user_age">Age: ${user.dob.age}</p>
+            </div>`;
+}
+
+function createUserCardActionsButtons () {
+    return `<div class="user_actions">` +
+        actionsButtonsImages
+            .map(actionImg => {
+                return `<img
+                    src="${actionImg.src}"
+                    alt=""
+                    class="user_actions_icon"
+                    id="${actionImg.id}"
+                />`;
+            })
+            .join("") +
+            `</div>`;
+}
+
+function createUserCardMoreInfo (user) {
+
+    return `<div class="more_user_info hide">
+                <p>
+                    <span class="extra_field">First name:</span>
+                    ${user.name.first}
+                </p>
+                <p>
+                    <span class="extra_field"
+                        >Second name:</span
+                    >
+                    ${user.name.last}
+                </p>
+                <p>
+                    <span class="extra_field"
+                        >Day OF Birth:</span
+                    >
+                    ${dobOfUser(user.dob.date)}
+                </p>
+                <p>
+                    <span class="extra_field">Location:</span>
+                    ${user.location.city}, ${user.location.country}
+                </p>
+            </div>`;
+}
+
+function dobOfUser (date) {
+    return ((new Date(date)).toDateString());
+}
+
+
 document.querySelector("#search_friends").addEventListener("click", (e) => {
-    document.querySelector("#filters_menu_btn").removeAttribute("disabled");
+    document.querySelector(".lds-ripple").classList.toggle("hide");
+    e.target.classList.remove("notclicked_yet");
+    document.querySelector("#filters_menu_btn").classList.remove("hide");
+    document.querySelector(".main_content").classList.remove("hide");
+    document.querySelector(".main").classList.remove("hide");
+    searchFriends();
 });
 
 document.querySelector("#filters_menu_btn").addEventListener("click", (e) => {
@@ -60,9 +172,8 @@ document.querySelector("#filters_menu_btn").addEventListener("click", (e) => {
     document
         .querySelector(".main_content")
         .classList.toggle("main_filter_hidden");
-    //document.querySelector(".main_info").classList.toggle("hide");
 });
-
+/*
 document.querySelectorAll(".disable_filter_btn").forEach((button) =>
     button.addEventListener("click", (e) => {
         e.target.parentNode
@@ -70,9 +181,17 @@ document.querySelectorAll(".disable_filter_btn").forEach((button) =>
             .forEach((input) => (input.checked = false));
     })
 );
+ */
+
+document.querySelectorAll('[name="sort_by_name"]').forEach(input => {
+    input.addEventListener('click', (e) => {
+        if (e.target.checked) {
+            document.querySelectorAll('[name="sort_by_name"]').forEach(o => o.checked = false);
+        }
+    })
+})
 
 document.querySelector(".found_users").addEventListener("click", (e) => {
-    console.log(e);
     if (e.target.getAttribute("id") === "user_actions_preview") {
         e.target.src = (e.target.classList.contains("active"))
             ? "./icons/icon-preview.png"
@@ -83,6 +202,5 @@ document.querySelector(".found_users").addEventListener("click", (e) => {
             .classList
             .toggle("hide");
         e.target.classList.toggle("active");
-
     }
 });
